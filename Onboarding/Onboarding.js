90cebe5f3848984a88848277dd34bafc3df394fe
Malith <malithgihan000@gmail.com>
2024-10-08 06:49:04 +0530

    onboarding

--- Onboarding/Onboarding.js
+++ Onboarding/Onboarding.js
@@ -67,6 +67,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: 'white'
   },
 });
 
--- Onboarding/OnboardingItem.js
+++ Onboarding/OnboardingItem.js
@@ -6,7 +6,7 @@ export default OnboardingItem = ({item}) => {
     const {width} = useWindowDimensions()
   return (
     <View style={[styles.container, {width}]}>
-      <Image source={item.image} style = {[styles.image,{width,resizeMode:'contain'}]}/>
+      <Image source={item.image} style = {[styles.image,{resizeMode:'contain'}]}/>
       <View style={{flex:0.3}}>
            <Text style={styles.title}>{item.title}</Text>
            <Text style={styles.description}>{item.description}</Text>
@@ -19,23 +19,25 @@ const styles = StyleSheet.create({
     container : {
         flex : 1,
         justifyContent : 'center',
-        alignItems : 'center'
+        alignItems : 'center',
+        backgroundColor:'white'
     },
 
     image : {
         flex : 0.7,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        width:'80%',
     },
     title: {
         fontWeight :'800',
         fontSize: 28,
-        marginBottom: 10,
-        color : '#493d8a',
+        marginBottom: 20,
+        color : '#003366',
         textAlign: 'center'
     },
     description : {
         fontWeight : '300',
-        color : '#62656b',
+        color : '#000',
         textAlign : 'center',
         paddingHorizontal : 64,
     }
